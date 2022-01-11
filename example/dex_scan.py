b1d9be5ca05e267d5a3b99d9b3e820754ed068fe
Matthew <matthew-ang@hotmail.com>
2022-01-11 11:31:55 +0800

    add docker example

--- example/dex_scan.py
+++ example/dex_scan.py
@@ -8,13 +8,24 @@ import asyncio
 from fx_py_sdk import scan
 import logging
 
+from fx_py_sdk.model.model import Sql
+import time
+
 async def main():
-    """rpc and websocket should run on the same time"""
+    """rpc and websocket should run at the same time"""
     logging.basicConfig(level=logging.INFO)
 
-    scan.RpcScan()
-    scan.WebsocketScan()
+    ws_scan = scan.WebsocketScan()
+    scan.RpcScan(ws_scan)
 
 if __name__ == "__main__":
+    """init database"""
+    sql = Sql(database="fxdex")
+    sql.create_table()  # creates tables if not exist
+
+    """give some time for db to fully init"""
+    time.sleep(10)
+
+    """begin scan"""
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
