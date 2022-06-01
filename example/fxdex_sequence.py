873962888d2c59655cafa2e1b627e27bed209cfa
ningxin21 <95473305+ningxin21@users.noreply.github.com>
2022-06-01 18:46:47 +0800

    Update fxdex_sequence.py
    fix: tps test
    
    fix: add time
    
    rm tps

--- example/fxdex_sequence.py
+++ example/fxdex_sequence.py
@@ -1,4 +1,7 @@
 import re
+import threading
+import time
+from typing import List
 
 from eth_account import Account
 from fx_py_sdk.grpc_client import GRPCClient
@@ -8,46 +11,100 @@ import decimal
 from fx_py_sdk.codec.cosmos.tx.v1beta1.service_pb2 import BROADCAST_MODE_BLOCK, BROADCAST_MODE_SYNC
 from google.protobuf.json_format import MessageToJson
 
+BASE_ASSET = "NFLX"
+mnemonic1 = "forum welcome cute hen dance winner bubble ski actor neutral usage cherry bullet play collect shift peasant step private grow arrive fade early alarm"
+mnemonic2 = "boss motor fall among end frame engage thing prosper hub divert scare turtle gift train general notice enemy task wedding tent kick unfold aspect"
+
 def main():
-    client = GRPCClient('127.0.0.1:9090')
+    client = GRPCClient(f"https://testnet-{BASE_ASSET.lower()}-grpc-normal.marginx.io:9090")
+    loop_number = 100
+    t0 = time.time()
 
-    Account.enable_unaudited_hdwallet_features()
-    account = Account.from_mnemonic(
-        "dune antenna hood magic kit blouse film video another pioneer dilemma hobby message rug sail gas culture upgrade twin flag joke people general aunt")
-    print(account.address)
+    def sell_order():
+        Account.enable_unaudited_hdwallet_features()
+        account = Account.from_mnemonic(mnemonic1)
+        print(account.address)
 
-    chain_id = client.query_chain_id()
-    print('chain_id:', chain_id)
+        chain_id = client.query_chain_id()
+        print('chain_id:', chain_id)
 
-    account_info = client.query_account_info(account.address)
-    print('account number:', account_info.account_number,
-          'sequence:', account_info.sequence)
+        account_info = client.query_account_info(account.address)
+        print('account number:', account_info.account_number,
+              'sequence:', account_info.sequence)
 
-    tx_builder = TxBuilder(account, None, chain_id, account_info.account_number, Coin(
-        amount='600', denom='USDT'))
+        tx_builder = TxBuilder(account, None, chain_id, account_info.account_number, Coin(
+            amount='600', denom='USDT'))
 
-    account_info = client.query_account_info(account.address)
-    print('account number:', account_info.account_number,
-          'sequence:', account_info.sequence)
-    sequence = account_info.sequence
-    for i in range(0, 10):
-        tx_response = client.create_order(tx_builder, 'TSLA:USDT', "SELL", decimal.Decimal(
-            900), decimal.Decimal(1.2), 5, sequence, mode=BROADCAST_MODE_SYNC)
+        account_info = client.query_account_info(account.address)
+        print('account number:', account_info.account_number,
+              'sequence:', account_info.sequence)
+        sequence = account_info.sequence
+        for i in range(0, loop_number):
+            tx_response = client.create_order(tx_builder, BASE_ASSET + ":USDT", "SELL", decimal.Decimal(
+                900), decimal.Decimal(1.2), 5, sequence, mode=BROADCAST_MODE_SYNC)
 
-        if tx_response.code == 20:
-            continue
-
-        if tx_response.code == 32:
-            if "account sequence mismatch" in tx_response.raw_log:
-                find = re.findall(r"expected ([0-9]+)", tx_response.raw_log)
-                sequence = int(find[0])
+            if tx_response.code == 20:
                 continue
 
-        res_str = MessageToJson(tx_response)
-        print(res_str)
-        sequence = sequence + 1
+            if tx_response.code == 32:
+                if "account sequence mismatch" in tx_response.raw_log:
+                    find = re.findall(r"expected ([0-9]+)", tx_response.raw_log)
+                    sequence = int(find[0])
+                    continue
+
+            res_str = MessageToJson(tx_response)
+            print(res_str)
+            sequence = sequence + 1
+
+    def buy_order():
+        Account.enable_unaudited_hdwallet_features()
+        account = Account.from_mnemonic(mnemonic2)
+        print(account.address)
+
+        chain_id = client.query_chain_id()
+        print('chain_id:', chain_id)
+
+        account_info = client.query_account_info(account.address)
+        print('account number:', account_info.account_number,
+              'sequence:', account_info.sequence)
+
+        tx_builder = TxBuilder(account, None, chain_id, account_info.account_number, Coin(
+            amount='600', denom='USDT'))
+
+        account_info = client.query_account_info(account.address)
+        print('account number:', account_info.account_number,
+              'sequence:', account_info.sequence)
+        sequence = account_info.sequence
+        for i in range(0, loop_number):
+            tx_response = client.create_order(tx_builder, BASE_ASSET + ":USDT", "BUY", decimal.Decimal(
+                900), decimal.Decimal(1.2), 5, sequence, mode=BROADCAST_MODE_SYNC)
+
+            if tx_response.code == 20:
+                continue
+
+            if tx_response.code == 32:
+                if "account sequence mismatch" in tx_response.raw_log:
+                    find = re.findall(r"expected ([0-9]+)", tx_response.raw_log)
+                    sequence = int(find[0])
+                    continue
+
+            res_str = MessageToJson(tx_response)
+            print(res_str)
+            sequence = sequence + 1
 
 
+    thread_list: List[threading.Thread] = []
+    t_sell = threading.Thread(target=sell_order)
+    t_buy = threading.Thread(target=buy_order)
+    thread_list.append(t_buy)
+    thread_list.append(t_sell)
+    t_sell.start()
+    t_buy.start()
+    for t in thread_list:
+        t.join()
+
+    t1 = time.time()
+    print(f"{t1 - t0:.3f} seconds elapsed")
 
 if __name__ == "__main__":
     main()
